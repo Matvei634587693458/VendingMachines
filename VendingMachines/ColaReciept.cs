17b171c54ad5b752d51de4654a35f94c001b6097
hhh <1067matvei@gmail.com>
2024-10-31 19:25:46 +0300

    Добавил кофиек

---
+++ VendingMachines/ColaReciept.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachines
+{
+    internal class ColaReciept
+    {
+        public string colaname { get; set; }
+
+        public int cola { get; set; }
+    }
+}
---
+++ VendingMachines/Colamat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachines
+{
+    class Colamat
+    {
+        public string machineName;
+        public int colaAmount { get; set; }
+
+        private List<ColaReciept> _cloaReciepts { get; set; }
+
+        public Colamat(List<ColaReciept> reciepts)
+        {
+            colaAmount = ColaOptions.maxColaCoffeeAmount;
+        }
+
+        public void PrintCoffee()
+        {
+            int count = 1;
+            Console.WriteLine("Кола");
+            foreach (ColaReciept coLa in _colaReciepts)
+            {
+                Console.WriteLine($"{count} {coLa.colaname}");
+                count++;
+            }
+
+
+        }
+        public void SellCoffee(int number)
+        {
+            number--;
+
+            if (number >= 0 && number < _colaReciepts.Count)
+            {
+                ColaReciept crnt = _colaReciepts[number];
+
+                if (colaAmount >= crnt.cola)
+                {
+                    colaAmount -= crnt.cola;
+
+                    Console.WriteLine($"{crnt.colaname} готово");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+        }
+    }
+}
